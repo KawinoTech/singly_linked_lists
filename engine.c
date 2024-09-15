4060e375941cb8302e9eecdecf6a9f4fe1216a3b
KawinoTech <jeremykawino@gmail.com>
2024-09-15 21:44:23 +0300

    fixed

---
+++ engine.c
@@ -0,0 +1,117 @@
+#include <stdbool.h>
+#include "main.h"
+/**
+ * prompt - Input data from the user
+ * engine - deals with routing of operations
+ * as per user choice
+ * @name: name of the Student
+ * @age: age of the Student
+ * @gender: gender of the Student
+ * @head: head to the linked list
+ *
+ * Return: 0 (Success)
+ * or 1 (Error)
+ */
+
+void prompt(char *name, int *age, char *gender);
+void engine(void)
+{
+	bool iterate;
+	size_t list_size;
+	char name[20], gender[10];
+	char *input_script;
+	int age, operation;
+	unsigned long int position;
+	Student *head;
+
+	iterate = true;
+	head = NULL;
+	input_script = "Choose operation\n1. Create new list"
+		"\n2. Insert new node at the beginning"
+		"\n3. Insert new node at position n"
+		"\n4. Insert new node at the end"
+		"\n5. Check all list items"
+		"\n6. Quit program\n\n";
+	while (iterate)
+	{
+		printf("%s", input_script);
+		scanf("%d", &operation);
+		switch (operation)
+		{
+			case 1:
+				if (head == NULL)
+				{
+					prompt(name, &age, gender);
+					new_list(name, age, gender, &head);
+				}
+				else
+				{
+					printf("<---Error--->\n List already exist\n<------>\n");
+				}
+				break;
+			case 2:
+				if (head != NULL)
+				{
+					prompt(name, &age, gender);
+					list_push(name, age, gender, &head);
+				}
+				else
+				{
+					printf
+					("<-Error->\n List does not exist\nPlease press 1 to create\n<--->\n");
+				}
+				break;
+			case 3:
+				printf("Enter Position: ");
+				scanf("%ld", &position);
+				if (head != NULL && position <= print_list(head))
+				{
+					prompt(name, &age, gender);
+					insert_n(name, age, gender, &head, position);
+				}
+				else
+				{
+					printf("<-----Error------>\n");
+				}
+				break;
+			case 4:
+				if (head != NULL)
+				{
+					prompt(name, &age, gender);
+					list_pop(name, age, gender, &head);
+				}
+				else
+				{
+					printf
+					("<-Error->\n List does not exist\nPlease press 1 to create\n<--->\n");
+				}
+				break;
+			case 5:
+				if (head != NULL)
+				{
+					list_size = print_list(head);
+					printf("-> %lu elements\n\n", list_size);
+				}
+				else
+				{
+					printf("\nEmpty List, Press 1 to create\n");
+				}
+				break;
+			case 6:
+				{
+					free_list(head);
+					exit(EXIT_SUCCESS);
+				}
+		}
+	}
+}
+
+void prompt(char *name, int *age, char *gender)
+{
+	printf("Enter your name: ");
+	scanf("%s", name);
+	printf("Enter your age: ");
+	scanf("%d", age);
+	printf("Enter your gender: ");
+	scanf("%s", gender);
+}
--- main.c
+++ main.c
@@ -1,106 +1,27 @@
-#include <stdbool.h>
 #include "main.h"
+/**
+ * main - Entry point of program
+ *
+ * Return: 0 (Success)
+ * or 1 (Error)
+ */
 
-void prompt(char *name, int *age, char *gender);
 int main(void)
 {
-	bool iterate;
-	size_t list_size;
-	char name[20], gender[10];
-	char *input_script;
-	int age, operation;
-	unsigned long int position;
-	Student *head;
+	unsigned int choice;
 
-	iterate = true;
-	head = NULL;
-	input_script = "Choose operation\n1. Create new list"
-		"\n2. Insert new node at the beginning"
-		"\n3. Insert new node at position n"
-		"\n4. Insert new node at the end"
-		"\n5. Check all list items"
-		"\n6. Quit program\n\n";
-	while (iterate)
+	printf("<---Welcome to DSA operations--->\n");
+	printf("PLease choose a Data Structure\n");
+	printf("1. Singly Linked Lists\n");
+	printf("2. Doubly Linked Lists\n");
+	scanf("%d", &choice);
+	if (choice == 1)
 	{
-		printf("%s", input_script);
-		scanf("%d", &operation);
-		switch (operation)
-		{
-			case 1:
-				if (head == NULL)
-				{
-					prompt(name, &age, gender);
-					new_list(name, age, gender, &head);
-				}
-				else
-				{
-					printf("<---Error--->\n List already exist\n<------>\n");
-				}
-				break;
-			case 2:
-				if (head != NULL)
-				{
-					prompt(name, &age, gender);
-					list_push(name, age, gender, &head);
-				}
-				else
-				{
-					printf
-					("<-Error->\n List does not exist\nPlease press 1 to create\n<--->\n");
-				}
-				break;
-			case 3:
-				printf("Enter Position: ");
-				scanf("%ld", &position);
-				if (head != NULL && position <= print_list(head))
-				{
-					prompt(name, &age, gender);
-					insert_n(name, age, gender, &head, position);
-				}
-				else
-				{
-					printf("<-----Error------>\n");
-				}
-				break;
-			case 4:
-				if (head != NULL)
-				{
-					prompt(name, &age, gender);
-					list_pop(name, age, gender, &head);
-				}
-				else
-				{
-					printf
-					("<-Error->\n List does not exist\nPlease press 1 to create\n<--->\n");
-				}
-				break;
-			case 5:
-				if (head != NULL)
-				{
-					list_size = print_list(head);
-					printf("-> %lu elements\n\n", list_size);
-				}
-				else
-				{
-					printf("\nEmpty List, Press 1 to create\n");
-				}
-				break;
-			case 6:
-				{
-					free_list(head);
-					exit(EXIT_SUCCESS);
-				}
-		}
+		engine();
+	}
+	else
+	{
+		printf("Sorry, Doubly linked list is not yet released\n");
 	}
 	return (0);
-}
-
-void prompt(char *name, int *age, char *gender)
-{
-	printf("Enter your name: ");
-	scanf("%s", name);
-	printf("Enter your age: ");
-	scanf("%d", age);
-	printf("Enter your gender: ");
-	scanf("%s", gender);
 }
